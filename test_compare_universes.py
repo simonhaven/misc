a9b076a07e5498b6c9605a618049f99ea83bb831
Simon Haven <64583094+simonhaven@users.noreply.github.com>
2023-12-03 19:45:10 +0000

    Add files via upload

--- test_compare_universes.py
+++ test_compare_universes.py
@@ -1,7 +1,6 @@
 import unittest
 import pandas as pd
 import numpy as np
-from pandas.util.testing import assert_frame_equal
 import compare_universes as cu
 
 class UniverseTest(unittest.TestCase):
